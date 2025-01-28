8849558591b8590f544fbcb0feca458222d23c20
irsou <irina.sousa00@gmail.com>
2025-01-28 20:12:51 +0100

    a

--- get_next_line_utils.h
+++ get_next_line_utils.h
@@ -6,12 +6,11 @@
 /*   By: isousa-s <isousa-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:03:26 by isousa-s          #+#    #+#             */
-/*   Updated: 2025/01/28 19:33:41 by isousa-s         ###   ########.fr       */
+/*   Updated: 2025/01/28 19:36:50 by isousa-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
 
 void	*ft_memset(void *ptr, int value, size_t length)
 {
@@ -19,7 +18,7 @@ void	*ft_memset(void *ptr, int value, size_t length)
 	unsigned char	*dir;
 
 	pos = 0;
-	dir = ptr;
+	dir = (unsigned char *)ptr;
 	while (pos < length)
 	{
 		dir[pos] = (unsigned char)value;
@@ -28,12 +27,10 @@ void	*ft_memset(void *ptr, int value, size_t length)
 	return (dir);
 }
 
-
 void	ft_bzero(void *ptr, size_t len)
 {
 	ft_memset(ptr, 0, len);
 }
-
 
 void	*ft_calloc(size_t num, size_t size)
 {

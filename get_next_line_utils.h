2cb0f5321c057450b267265f275f8a93c37ae198
irsou <123366527+irsou@users.noreply.github.com>
2025-01-28 19:22:47 +0100

    Create get_next_line_utils.h

---
+++ get_next_line_utils.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isousa-s <isousa-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 18:03:26 by isousa-s          #+#    #+#             */
+/*   Updated: 2025/01/28 19:13:00 by isousa-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_calloc(size_t num, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(num * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, num * size);
+	return (ptr);
+}

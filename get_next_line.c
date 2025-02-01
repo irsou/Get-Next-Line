ec0b3f441654402de3afd7182246a8e8e2c59070
irsou <123366527+irsou@users.noreply.github.com>
2025-02-01 10:02:39 +0100

    empezamos de cero

--- get_next_line.c
+++ get_next_line.c
@@ -6,56 +6,36 @@
 /*   By: isousa-s <isousa-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:01:16 by isousa-s          #+#    #+#             */
-/*   Updated: 2025/01/28 19:32:17 by isousa-s         ###   ########.fr       */
+/*   Updated: 2025/02/01 09:55:12 by isousa-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*read_line(int fd, char *pending_text)
-{
-	int		len;
-	char	*tmp;
-
-	if (!pending_text)
-		pending_text = ft_calloc(1, 1);
-	tmp = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
-	len = 1;
-	while (len > 0)
-	{
-		len = read(fd, tmp, BUFFER_SIZE);
-		if (len == -1)
-		{
-			free(tmp);
-			free(pending_text);
-			return (NULL);
-		}
-		tmp[len] = '\0';
-		pending_text = ft_free_strjoin(pending_text, tmp);
-		if (ft_strchr(pending_text, '\n'))
-			break ;
-	}
-	free(tmp);
-	return (pending_text);
-}
-
 char	*get_next_line(int fd)
 {
+	static char	*storage;
+	char		*buffer;
 	char		*line;
-	static char	*pending_text;
+	int			bytes_read;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if(fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	pending_text = read_line(fd, pending_text);
-	if (pending_text == NULL)
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE +1 ));
+	if(!buffer)
 		return (NULL);
-	line = get_line(pending_text);
-	pending_text = cut_line(pending_text);
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	buffer[bytes_read]= '\0';
+	line=buffer;
 	return (line);
-}
 
-/*read_until_enter  - read_line
-make_line_from - get_line
-the_rest - cut_line 
-save - pending_text
-*/
+
+	storage = NULL;
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isousa-s <isousa-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 09:26:40 by isousa-s          #+#    #+#             */
+/*   Updated: 2025/02/01 09:36:36 by isousa-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -8,8 +20,8 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-void	*ft_calloc(size_t num, size_t size);
+// void	*ft_calloc(size_t num, size_t size);
 char	*get_next_line(int fd);
-char	*read_line(int fd, char *pending_text);
+// char	*read_line(int fd, char *pending_text);
 
 # endif
